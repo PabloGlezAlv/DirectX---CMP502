58c0ad64fa062b8b6cf89fc792f7e17914197d4f
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2024-03-25 15:22:57 +0100

    Project files

---
+++ source/Input.h
@@ -0,0 +1,43 @@
+#pragma once
+
+struct InputCommands
+{
+	bool movForward;
+	bool movBack;
+	bool movLeft;
+	bool movRight;
+	bool movUp;
+	bool movDown;
+
+	bool mouseLeft;
+	bool mouseRight;
+
+	float deltaX;
+	float deltaY;
+
+	bool B;
+	bool H;
+};
+
+
+class Input
+{
+public:
+	Input();
+	~Input();
+	void Initialise(HWND window);
+	void Update();
+	bool Quit();
+	InputCommands getGameInput();
+
+
+private:
+	bool m_quitApp;
+
+	std::unique_ptr<DirectX::Keyboard>	m_keyboard;
+	std::unique_ptr<DirectX::Mouse>		m_mouse;
+	DirectX::Keyboard::KeyboardStateTracker m_KeyboardTracker;
+	DirectX::Mouse::ButtonStateTracker		m_MouseTracker;
+	InputCommands							m_GameInput;
+};
+
